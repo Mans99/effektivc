143a9eba06558f4c0742d3891ab1036232e28421
Mans99 <mans.englund@live.se>
2024-11-14 13:09:57 +0100

    fixed issues

--- lab2/simplex.c
+++ lab2/simplex.c
@@ -26,9 +26,6 @@ int init(struct simplex_t* s, int m, int n, int* var, double** A, double* b, dou
     if (s->var == NULL) {
         s->var = calloc(m + n, sizeof(int));
         for (i = 0; i < m + n; i++) {
-    if (s->var == NULL) {
-        s->var = calloc(m+m+1, sizeof(int));
-        for (i = 0; i < m+n; i=i+1) {
             s->var[i] = i;
         }
     }
@@ -42,6 +39,7 @@ int init(struct simplex_t* s, int m, int n, int* var, double** A, double* b, dou
     }
     return k;
 }
+
 
 int select_nonbasic(struct simplex_t* s){
     int i;
@@ -114,3 +112,61 @@ int initial(struct simplex_t* s,int m,int n,int* var, double** A, double* b, dou
 }
 
 */
+
+
+int main(int argc, char** argv) {
+    int m, n;
+    double* c;
+    double** a;
+    double* b;
+    int i, j;
+    struct simplex_t simplex;
+    int* var = NULL;  // Initialize as NULL to be allocated in `init()`
+    double y = 0;
+    double* x = NULL;  // This can remain NULL if not used in the current logic
+
+    // Read m and n
+    scanf("%d %d", &m, &n);
+    printf("m = %d, n = %d\n", m, n);
+
+    // Allocate memory for c, a, and b
+    c = calloc(n, sizeof(double));
+    a = calloc(m, sizeof(double*));
+    for (i = 0; i < m; i++) {
+        a[i] = calloc(n, sizeof(double));
+    }
+    b = calloc(m, sizeof(double));
+
+    // Read coefficients of the objective function c
+    for (i = 0; i < n; i++) {
+        scanf("%lf", &c[i]);
+    }
+
+    // Read matrix A
+    for (i = 0; i < m; i++) {
+        for (j = 0; j < n; j++) {
+            scanf("%lf", &a[i][j]);
+        }
+    }
+
+    // Read vector b
+    for (i = 0; i < m; i++) {
+        scanf("%lf", &b[i]);
+    }
+
+    // Initialize the simplex structure
+    int min_index = init(&simplex, m, n, var, a, b, x, c, y);
+    printf("Index of minimum b[i]: %d\n", min_index);
+    printf("Minimum b value: %lf\n", b[min_index]);
+
+    // Free allocated memory
+    free(c);
+    for (i = 0; i < m; i++) {
+        free(a[i]);
+    }
+    free(a);
+    free(b);
+    free(simplex.var);
+
+    return 0;
+}
